a3855dc88e11a0dc45fde9df1b64caebbd5bb34e
Meghann McCool Walsh <meghann.mccool.walsh@gmail.com>
2019-07-10 13:22:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -56,14 +56,36 @@ def game_hash
 :assists => 2
 :steals => 4
 :blocks => 11
-:slam_dunks => 1}
+:slam_dunks => 1},
 
-  }
-  :team_name =>
-  :colors => []
-  :players => [{}]
-},
+  },
 :away => {
+  :team_name => "Charoltte Hornets"
+  :colors => ["Turqoise", "Purple"]
+  :players => [{
+    :name => "Jeff Adrien"
+    :number => 4
+    :shoe => 18
+    :points => 10
+    :rebounds => 1
+    :assists => 1
+    :steals => 2
+    :blocks => 7
+    :slam_dunks => 2
+    }
+    {
+      :name => "Bismack Biyombo"
+      :number => 0
+      :shoe => 16
+      :points => 12
+      :rebounds => 4
+      :assists => 7
+      :steals => 22
+      :blocks => 15
+      :slam_dunks => 10
+    }
+  ]
+}
 }
 ]
 end
